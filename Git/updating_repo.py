30d14d8b8cac373a8e68e1c44c3e4b6812b82118
Igwesi Emmanuel <emmaigwesi@gmail.com>
2022-04-04 13:18:22 +0100

    First commit

--- Git/updating_repo.py
+++ Git/updating_repo.py
@@ -3,10 +3,12 @@ from os import system
 
 try:
     system("git add -A")
-    time.sleep(5)
+    time.sleep(2)
     message = input("Enter commit Message: ")
-    system(f"git commit -m {message}")
-    time.sleep(5)
+    system(f'git commit -m "{message}"')
+    time.sleep(2)
     system("git push -u origin master")
+    time.sleep(5)
+    print("successfully pushed")
 except Exception as e:
     print(e)
