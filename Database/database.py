ccba033ede263a15c3e813bcad30d820116e9c54
Igwesi Emmanuel <emmaigwesi@gmail.com>
2022-04-05 13:30:26 +0100

    Updated

--- Database/database.py
+++ Database/database.py
@@ -28,6 +28,7 @@ cursor = conn.cursor()
 # # Create single data
 # cursor.execute("INSERT INTO customers VALUES ('John','Elder', 'john_elder@gmail.com')")
 
+# Delete 
 cursor.execute("DELETE FROM customers WHERE first_name ='John'")
 
 conn.commit()
