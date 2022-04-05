44fb036539d1914d9539699a400c1427311bccec
Igwesi Emmanuel <emmaigwesi@gmail.com>
2022-04-05 11:44:26 +0100

    Commit

--- Git/update_repo.py
+++ Git/update_repo.py
@@ -1,19 +1,21 @@
 import time
 from os import system
+import webbrowser
+
+url = "git"
+
+webbrowser.open(url)
 
 try:
+    message = input("Enter commit Message: ")
     print("Adding all files...")
     system("git add -A")
     print("Files added")
-    # time.sleep(2)
-    message = input("Enter commit Message: ")
     print("Committing...")
     system(f'git commit -m "{message}"')
     print("Committed")
-    # time.sleep(2)
     print("Pushing repo")
     system("git push -u origin master")
-    # time.sleep(5)
     print("Repo successfully Updated")
 except Exception as e:
     print(e)
